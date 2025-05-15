0a6c06ae1df4d575dc607cf459c0e8567ef8794d
corak123 <advanadesain@gmail.com>
2025-05-15 14:45:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if barang_list:
                         keterangan=keterangan
                     )
 
-                    if "berhasil" in str(hasil.lower()):
+                    if isinstance(hasil, str) and "berhasil" in hasil.lower():
                         update_result = update_sisa_barang(
                             invoice_id, selected["kode_barang"], int(jumlah_keluar)
                         )
